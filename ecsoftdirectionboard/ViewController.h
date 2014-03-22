efbc7ca4a2b573dd1abb6f808e7ae0dbaa0ff083
Kalyan Kumar <kalyankumar@Kalyans-Mac.local>
2014-03-22 08:05:11 +0530

    Initial Commit

---
+++ ecsoftdirectionboard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ecsoftdirectionboard
+//
+//  Created by Kalyan Kumar on 22/03/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ecsoftdirectionboard/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ecsoftdirectionboard
+//
+//  Created by Kalyan Kumar on 22/03/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
